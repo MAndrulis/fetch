c6dbbffe7a7b921c12513382bec7c5310ba9eb60
Modestas And <modestas.andrulis@gmail.com>
2023-06-26 13:08:03 +0200

    fetch

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
-console.log('fetch demo...');
-console.log('fetch demo...');
-console.log('fetch demo...');
-console.log('fetch demo...');
+console.log('START');
+
+const response = fetch('http://localhost:4514/data/marks.json')
+    .then((res) => res.json())
+    .then((data) => {console.log(data) });
+
+console.log(response);
+
+
+
+
+console.log('END');
